110d6246e114c20e3abf8f4855dc2ba87aaa5243
Opal <j.opal.may@gmail.com>
2016-10-01 23:21:50 -0400

    First try at Stormpath authentication

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var stormpath = require('express-stormpath');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
@@ -13,7 +14,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://medsafe_lab:elixir-vitae@ds021711.mlab.com:21711/medsafe_management',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'medsafe-manager',
+  appId: process.env.APP_ID || '30nPZe2ThHA0PWQT0O0CA3s29Kil2shkOzw73NHn',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://medsafe-manager.herokuapp.com/',  // Don't forget to change to https if needed
 });
@@ -27,9 +28,27 @@ var app = express();
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
+//Initialize stormpath
+app.use(stormpath.init(app, { website: true }));
+
+app.on('stormpath.ready', function() {
+  app.listen(process.env.PORT || 3000);
+});
+
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being an opalescent web site.');
+});
+
+app.get('/validate', function(req, res) {
+  try {
+    var incomingToken = jwt.verify(req.query.token, "smart is sexy");
+  } catch (ex) {
+    console.error(ex.stack);
+    return res.status(401).send('jwt error');
+  }
+  
+  
 });
 
 var port = process.env.PORT || 1337;
