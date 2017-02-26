6bde1acd6012f8fd81289629268ef97e67263348
Opal <j.opal.may@gmail.com>
2017-02-26 14:25:08 -0500

    Tyring again to enable LiveQuery

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'medsafe-manager',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://medsafe-manager.herokuapp.com/',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Message"] // List of classes to support for query subscriptions
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -62,7 +65,17 @@ app.on('stormpath.ready', function() {
   app.listen(process.env.PORT || 3000);
 });
 
-//var port = process.env.PORT || 1337;
-//app.listen(port, function() {
-//    console.log('parse-server-example running on port ' + port + '.');
-//});
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
